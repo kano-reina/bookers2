ab4f1243673b37f83a54e40672b874b8005ba15a
EC2 Default User <ec2-user@ip-172-31-15-55.ap-northeast-1.compute.internal>
2024-04-20 09:20:14 +0000

    [Start]Bookers2_42

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,11 +28,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
-    redirect_to book_path(book.id)
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
   
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
   
 end
